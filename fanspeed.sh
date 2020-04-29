56eb73cf455ed3578b6e65e01f8c5d8c3d69a9c7
tomp736 <tompisula@gmail.com>
2020-04-29 13:26:45 +0200

    add int -> hex

--- fanspeed.sh
+++ fanspeed.sh
@@ -1,7 +1,23 @@
-#!bin/sh
+#!bin/bash
+
+local intSpeed = 85
+local hexSpeed = $(printf '%x\n' $hexSpeed)
 
 #require ipmi tools for manual fanspeed management
 command -v ipmitool >/dev/null 2>&1 || { echo >&2 "I require imptool but it's not installed.  Aborting."; exit 1; }
 
+#enable manual fan speed control
 sudo ipmitool raw 0x30 0x30 0x01 0x00
-sudo ipmitool raw 0x30 0x30 0x02 0xff 0x14
+
+#set fanspeed
+echo "Setting fans to $intSpeed %"
+sudo ipmitool raw 0x30 0x30 0x02 0xff $hexSpeed
+
+#  % = HEX
+# 10 = A
+# 20 = 14
+# 30 = 1E
+# 40 = 28
+# 50 = 32
+# 60 = 3C
+# 70 = 46
