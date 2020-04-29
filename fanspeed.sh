972b1a72298fad1292b8b6c6d660ed5e6477ccec
tomp736 <tompisula@gmail.com>
2020-04-29 13:45:13 +0200

    1

--- fanspeed.sh
+++ fanspeed.sh
@@ -2,27 +2,32 @@
 
 setFanSpeed()
 {
+    local minSpeed=0
+    local maxSpeed=100
+
     local intSpeed=$1
+    local isValid=false
+    if [[ "$intSpeed" =~ ^[0-9]+$ ]] then
+        if [[ $intSpeed -ge 20 &&  $intSpeed -le 100 ]] then
+            isValid=true
+        fi
+    fi
+
+    if ! [[ $isValid ]] then
+        echo "$intSpeed is not a valid fanspeed. Valid range is 20% - 99% because my ears."; exit 1;
+    fi
+
     local hexSpeed=$(printf '%x\n' $hexSpeed)
 
     #require ipmi tools for manual fanspeed management
     command -v ipmitool >/dev/null 2>&1 || { echo >&2 "I require imptool but it's not installed.  Aborting."; exit 1; }
 
     #enable manual fan speed control
-    # sudo ipmitool raw 0x30 0x30 0x01 0x00
+    sudo ipmitool raw 0x30 0x30 0x01 0x00
 
     #set fanspeed
     echo "Setting fans to $intSpeed %"
-    # sudo ipmitool raw 0x30 0x30 0x02 0xff $hexSpeed
+    sudo ipmitool raw 0x30 0x30 0x02 0xff $hexSpeed
 }
 
 setFanSpeed 85
-
-#  % = HEX
-# 10 = A
-# 20 = 14
-# 30 = 1E
-# 40 = 28
-# 50 = 32
-# 60 = 3C
-# 70 = 46
