68662c54b6f5982e69be27e27bfabfec0cbf95dc
tomp736 <tompisula@gmail.com>
2020-04-29 13:34:37 +0200

    1

--- fanspeed.sh
+++ fanspeed.sh
@@ -2,8 +2,8 @@
 
 setFanSpeed()
 {
-    local intSpeed = 85
-    local hexSpeed = $(printf '%x\n' $hexSpeed)
+    local intSpeed=85
+    local hexSpeed=$(printf '%x\n' $hexSpeed)
 
     #require ipmi tools for manual fanspeed management
     command -v ipmitool >/dev/null 2>&1 || { echo >&2 "I require imptool but it's not installed.  Aborting."; exit 1; }
